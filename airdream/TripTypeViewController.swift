e06127517725061f8f75b692e4cbdffe610faa39
Bastien DAVID <bastien.david@orange.com>
2018-09-27 21:23:05 +0200

    Simplified flow - only one step left

--- airdream/TripTypeViewController.swift
+++ airdream/TripTypeViewController.swift
@@ -8,17 +8,31 @@
 
 import UIKit
 
-class TripTypeViewController: UIViewController {
+class TripTypeViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var tripType: UISwitch!
+    @IBOutlet weak var departureCity: UITextField!
+    @IBOutlet weak var arrivalCity: UITextField!
+    
+    override func viewDidLoad() {
+        departureCity.delegate = self
+        arrivalCity.delegate = self
+    }
 
     @IBAction func onNext(_ sender: UIButton) {
 
         let wizard = navigationController as! BookingWizardNavigationViewController
         wizard.tripType = tripType.isOn ? 2 : 1
+        wizard.departureCity = departureCity.text!
+        wizard.arrivalCity = arrivalCity.text!
 
         let storyboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
-        let departureCityViewController: DepartureCityViewController = storyboard.instantiateViewController(withIdentifier: "DepartureCityViewController") as! DepartureCityViewController
-        navigationController?.pushViewController(departureCityViewController, animated: true)
+        let bookingConfirmationViewController: BookingConfirmationViewController = storyboard.instantiateViewController(withIdentifier: "BookingConfirmationViewController") as! BookingConfirmationViewController
+        navigationController?.pushViewController(bookingConfirmationViewController, animated: true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
 }
